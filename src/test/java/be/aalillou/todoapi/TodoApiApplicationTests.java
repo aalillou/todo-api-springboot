2c1cf937171d614449e4410b609cdc02becfb037
Mo <mo.aalillou@dxc.com>
2023-03-19 09:06:11 +0100

    first commit

---
+++ src/test/java/be/aalillou/todoapi/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.aalillou.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
